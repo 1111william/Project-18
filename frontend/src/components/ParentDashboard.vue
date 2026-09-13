4f86d12610386040fd7c8d0a4ba4db348149cc5c
Jassim313 <102858285@student.swin.edu.au>
2026-09-13 22:41:44 +1000

    Add initial Parent Dashboard component
    Create initial Parent Mode structure for child progress and learning information.

---
+++ frontend/src/components/ParentDashboard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Parent Dashboard</h1>
+
+    <h2>Children</h2>
+    <p>Child progress will be shown here.</p>
+
+    <h2>Learning Progress</h2>
+    <p>Quiz results and learning activity will be shown here.</p>
+  </div>
+</template>
